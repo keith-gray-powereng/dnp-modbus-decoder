11e4e2ba4d60652b0d593540e744456d7064badc
Andy Holdener <duckmaster09@gmail.com>
2013-04-26 10:07:03 -0500

    Adding our application's main Django code in the PowerDecoder folder

---
+++ source code/PowerDecoder/Django Webpage/DNP3_Decoder/views.py
@@ -0,0 +1,19 @@
+# Create your views here.
+from django.http import HttpResponse #Needed?
+from django.shortcuts import render
+#from POC import DNP3 #Contains our DNP3 decoder module
+
+
+def index(request):
+	return render(request, 'DNP3_Decoder/input.html')
+	
+def auto(request):
+	return render(request, 'DNP3_Decoder/auto.html')
+
+def dnp3(request):
+	#return HttpResponse(DNP3())
+	return HttpResponse("Output Basic Decoded message here")
+	#return render(request, 'DNP3_Decoder/dnp3.html')
+
+def modbus(request):
+	return render(request, 'DNP3_Decoder/modbus.html')
