9501a632f0280315e18736ed74163d00035a0d8e
Westin Breger <wbreger@siue.edu>
2013-09-02 15:12:06 -0500

    Getting the tests going

--- source code/PowerDecoder/Django Webpage/DNP3_Decoder/tests.py
+++ source code/PowerDecoder/Django Webpage/DNP3_Decoder/tests.py
@@ -1,16 +1,26 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+This file will likely not run.
+Test are written first, and from these test, the actual functions are made
 """
 
 from django.test import TestCase
+import bitstring
+import POC
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        #do stuff here to setup tests
+        #POC does not require a standard setup
+        pass
+        
+    def test_sliceTestFail(self):
+        testWord = bitstring.Bits("0x0000F0000")
+        result = slice(testWord, 0, 2)
+        assert result == bitstring.Bits("0x0") , "slice somehow returned the correct result"
+        
+    def test_sliceTestPass(self):
+        testWord = bitstring.Bits("0x0000FF0000")
+        result = slice(testWord, 8, 2)
+        assert result == bitstring.Bits("0xFF") , "slice is pulling the wrong bits"    
+        
