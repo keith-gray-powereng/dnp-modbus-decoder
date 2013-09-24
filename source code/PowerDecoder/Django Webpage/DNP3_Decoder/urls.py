d1eed029d66f21d101aa0e844457dd3abbe652cc
Andy Holdener <duckmaster09@gmail.com>
2013-09-24 01:09:49 -0500

    Adding functionality to the DNP3 page.  It can now display the decoded DNP message on our template page.

--- source code/PowerDecoder/Django Webpage/DNP3_Decoder/urls.py
+++ source code/PowerDecoder/Django Webpage/DNP3_Decoder/urls.py
@@ -5,6 +5,7 @@ import views
 urlpatterns = patterns('',
 	url(r'^index', views.index, name='index'),
 	url(r'^dnp3/', views.dnp3, name='dnp3'), #Partially implemented
+	url(r'^output/', views.dnp3, name='output'), #Not sure if needed
 	url(r'^modbus/', views.modbus, name='modbus'), #Not implemented yet
-	url(r'^auto/', views.auto, name='auto') #Not implemented yet
+	url(r'^auto/', views.auto, name='auto') #Not implemented yet	
 )
--- source code/PowerDecoder/Django Webpage/DNP3_Decoder/views.py
+++ source code/PowerDecoder/Django Webpage/DNP3_Decoder/views.py
@@ -1,9 +1,7 @@
-
 # Create your views here.
 from django.http import HttpResponse #Needed?
-from django.shortcuts import render
+from django.shortcuts import render, RequestContext, loader
 import POC #Contains our DNP3 decoder module
-import BitSlice
 
 def index(request):
 	return render(request, 'DNP3_Decoder/basicTemplate.html')
@@ -12,14 +10,18 @@ def auto(request):
 	return render(request, 'DNP3_Decoder/auto.html')
 
 def dnp3(request):
-	decodedObj = POC.DNP3("05 64 05 C0 01 00 0A 00")
-	outty = ""
+	#if 'msg' in request.GET and request.GET['msg']:
+	userData = request.GET['msg']
+	decodedObj = POC.DNP3(str(userData))#("05 64 05 C0 01 00 0A 00")
+	outty=[]
 	for i in decodedObj:
-		for l in i:
-			outty += str(l) + " "
-		outty += "</p>"
-	return HttpResponse(outty)
-	#return render(request, 'DNP3_Decoder/dnp3.html')
+		outty.append(str(i))
+	#outty += str(i) + "</p>"
+	#return HttpResponse(outty)
+	return render(request, 'DNP3_Decoder/output.html', {'decodedStuff':outty, 'originalMsg':str(userData)}) #decodedObj = dictionary
+	template = loader.get_template('DNP3_Decoder/output.html')
+	context = RequestContext(request, {'decodedStuff':outty, 'originalMsg':str(userData)})
+	return HttpResponse(template.render(context))
 
 def modbus(request):
 	return render(request, 'DNP3_Decoder/modbus.html')
