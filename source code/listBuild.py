6301425a90b94ce2874f2a3ca592d860ba091659
sjarnag <sjarnag@siue.edu>
2013-11-19 19:42:03 -0600

    updated report build

--- source code/listBuild.py
+++ source code/listBuild.py
@@ -1,35 +1,35 @@
-"""File: listBuild"""
-"""Takes the user's input or specified text file and breaks it down into a list
+'''File: listBuild'''
+'''Takes the user's input or specified text file and breaks it down into a list
 of the individual messages (whether DNP3 or Modbus) which is then returned.
 NOTE: if both input and a specified text file are provided, this module will
-attempt to use the file first, but will default to the input if file input fails"""
+attempt to use the file first, but will default to the input if file input fails'''
 import Report.py
 
 def makeList(reportList):
-	"""Currently assuming that it will be passed rep, which is a list of sperate messages"""
-	myList = '<ul class="collapsibleList" id="mahList">\n'
+	'''Currently assuming that it will be passed rep, which is a list of sperate messages'''
+	outputString = '<ul class="collapsibleList" id="mahList">\n'
 	
-	for msg in reportList: #loop through each Report msg#= #FIGURE OUT IF LIST OR DUMMY HEAD
-		#if msg.description != "":
-			#myList += msg.description
+	for report in reportList: #loop through each Report report#= #FIGURE OUT IF LIST OR DUMMY HEAD
+		#if report.description != "":
+			#outputString += report.description
 		#else:
-		myList += msg.title + ": " + msg.data
-		myList = innerContents(msg.Next, myList)
+		outputString += report.title + ": " + report.data
+		outputString = innerContents(report.Next, outputString)
 	#end for
-	myList += '\n</ul>'
-	return myList
+	outputString += '\n</ul>'
+	return outputString
 
-def innerContents(reportList, myList):
-	"""description"""
+def innerContents(reportList, outputString):
+	'''description'''
 	for item in reportList:
-		myList += "<li>\n"
-		myList += item.title + ": " + item.data + "\n"
-		if item.description == "":
-			myList += "<li>\n" + item.description + "\n</li>\n</li>\n"
+		outputString += "<li>\n"
+		outputString += item.title + ": " + item.data + "\n"
+		if item.description != "":
+			outputString += "<li>\n" + item.description + "\n</li>\n</li>\n"
 		else:
-			myList += "<ul>\n"
-			myList = innerContents(reportList.Next, myList)
-			myList += "</ul>\n"
+			outputString += "<ul>\n"
+			outputString = innerContents(reportList.Next, outputString)
+			outputString += "</ul>\n"
 	#end for
-	myList += "</li>\n"
-	return myList
+	outputString += "</li>\n"
+	return outputString
