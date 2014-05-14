be03d158dc4926cffbbae12af0cd186f865e8190
Keith Gray <keith.gray@powereng.com>
2014-05-13 19:29:49 -0600

    Fixed line endings in DNPReportBuilder

--- DNP3/DNPReportBuilder.py
+++ DNP3/DNPReportBuilder.py
@@ -11,12 +11,12 @@ class DNPReportBuilder:
 
     def dummyData(self):
         return [("05 64 12 C4 02 00 64 00 FF B7", False), ("F8 C8 05 29 02 28 01 00 10 00 00 80 00 E8 57", False), ("05 64 14 44 64 00 02 00 A4 D8", False), ("F1 C8 81 00 00 29 02 28 01 00 10 00 00 80 00 7E 25", False)]
-    
+
     def __init__(self):
         self.out = Report("Message", "", "")
         self.typeDict = buildDict()
-        
-    
+
+
     '''Translates message into a displayable report
     I am expecting "message" to be a tuple with hex numbers, preferrably strings, and a bool referring to if it is a request or not'''
     def translate(self, message):
@@ -41,7 +41,7 @@ class DNPReportBuilder:
             temp.append(i)
         if len(temp) > 0:
             hexMessages.append(list(temp))
-           
+
         index = -1
         allMessages = Report("All Messages", "The entirity of the message", "", "")
         messageCount = -1
@@ -52,7 +52,7 @@ class DNPReportBuilder:
             for msg in hexMessage:
                 hexString += msg.hex + ', '
             thisMessage = Report("Message {}".format(messageCount), "", "", hexString)
-                       
+
             #verify correctness
             try:
                 if not DataLinkTranslator.DataLayerCorrect(hexMessage[0]):
@@ -60,29 +60,29 @@ class DNPReportBuilder:
                     return Report("Invalid Message", "", "", "")
             except:
                 return Report("Invalid Input", "", "", "")
-                
+
             #remove CRC bits for everything (Andy took 'em out)
             #for i in hexMessage:
             #    i = DataLinkTranslator.StripCRCBits(i)
-            
+
             #Get message length
             thisMessage.length = DataLinkTranslator.DataLayerLength(hexMessage[0][:])
             thisMessage.MessageLength = thisMessage.length.uint
             thisMessage.AddNext(Report("Message length", "Number of octets this message contains that are not CRC related", str(thisMessage.length.uint), ""))
-            
+
             #Get Control Field
             control = DataLinkTranslator.DataLayerControl(hexMessage[0][:])
             thisMessage.AddNext(Report("Message Control Data", "Function operations and qualifiers", str(control.hex), ""))
             thisMessage.Next[-1].AddNext(DataLinkTranslator.DataLayerControlReport(control))
-            
+
             #message sender
             thisMessage.sender = DataLinkTranslator.DataLayerSource(hexMessage[0][:])
             thisMessage.AddNext(Report("Message Sender", "ID for sender", str (thisMessage.sender.hex), ""))
-            
+
             #message reciever
             thisMessage.reciever = DataLinkTranslator.DataLayerDestination(hexMessage[0][:])
             thisMessage.AddNext(Report("Message Reciever", "ID for Reciever", str(thisMessage.reciever.hex), ""))
-            
+
             #message transport layer
             thisMessage.transport = ""
             try:
@@ -95,12 +95,12 @@ class DNPReportBuilder:
                 hexMessage[1] = hexMessage[1][8:]
             except:
                 print("decoding a response message\n")
-            
+
             #technically a block, so sue me
             fragment = 1
             baseLayer = thisMessage
             while fragment < len(hexMessage) :
-            
+
                 bucket = []
                 #requests contain no actual data
                 #just outlines for what is expected in responses
@@ -117,25 +117,25 @@ class DNPReportBuilder:
                     bucket.append(Report("Object Header", "Prefix information on Application layer", "", ""))
                     flags = getAppRequestHeader(hexMessage[fragment])
                     for i in flags:
-                        bucket[-1].Next.append(i) 
+                        bucket[-1].Next.append(i)
                     bucket.append(translateFuncCode(getFuncCode(hexMessage[fragment])))
-                    
-                    
+
+
                 for i in bucket:
                     temp = i
                     if not isinstance(i, Report):
                         temp = Report(i,"","","")
                     baseLayer.Next[-1].AddNext(temp)
-                    
+
                 baseLayer.AddNext(Report("Application Fragment {}".format(fragment -1), "", "", ""))
-                    
+
                 fragment += 1
-                
+
             allMessages.AddNext(thisMessage)
-               
+
         print (allMessages)
         return allMessages
-        
+
         #http://www.kepware.com/Support_Center/SupportDocuments/DNP3_Control_Relay_Output_Block.pdf
 
 
