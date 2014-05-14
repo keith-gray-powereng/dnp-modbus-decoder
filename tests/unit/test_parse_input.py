ab07d89c071e845fee99feafcf17b366fae6dc72
Keith Gray <keith.gray@powereng.com>
2014-05-13 19:29:27 -0600

    Fixed tests to not remove spaces prior to calling parseInput.parseData

--- tests/unit/test_parse_input.py
+++ tests/unit/test_parse_input.py
@@ -25,27 +25,27 @@ class TestParseInput(unittest.TestCase):
     def test_pasted_tx_message_with_message_bytes_and_parens_around_crc(self):
         'Verifies the result for a pasted message with parenthesis around the crc bytes'
         input_data = '05 64 05 C0 01 00 0A 00 (E0 8C)'
-        input_data = input_data.replace(' ', '')
-        paren_location = input_data.find('(')
-        output_data = [(input_data[:paren_location], "True")]
+        output_data = input_data.replace(' ', '')
+        paren_location = output_data.find('(')
+        expected_output = [(output_data[:paren_location], "True")]
         exit_value = parseInput.parseData(input_data, '')
-        self.assertEqual(exit_value, output_data)
+        self.assertEqual(exit_value, expected_output)
 
     def test_pasted_tx_message_with_message_bytes_and_parens_around_crc_and_leading_tx(self):
         'Verifies the result for a pasted message with leading TX[xx]: and parenthesis around the crc bytes'
         input_data = 'TX[10]: 05 64 05 C0 01 00 0A 00 (E0 8C)\n'
-        input_data = input_data.replace(' ', '')
-        open_paren_location = input_data.find('(')
-        close_paren_location = input_data.find(')')
-        colon_location = input_data.find(':')
-        output_data = [
+        output_data = input_data.replace(' ', '')
+        open_paren_location = output_data.find('(')
+        close_paren_location = output_data.find(')')
+        colon_location = output_data.find(':')
+        expected_output = [
             (
-                input_data[colon_location + 1:open_paren_location],
-                input_data[open_paren_location + 1:close_paren_location],
+                output_data[colon_location + 1:open_paren_location],
+                output_data[open_paren_location + 1:close_paren_location],
                 "True"
             )]
         exit_value = parseInput.parseData(input_data, '')
-        self.assertEqual(exit_value, output_data)
+        self.assertEqual(exit_value, expected_output)
 
     def test_file_message(self):
         'Verifies the result for a series of messages from a file'
@@ -87,8 +87,8 @@ class TestParseInput(unittest.TestCase):
             '(Port 1):  \n'
             '(Port 1): Reset Remote Link - Device 2 Address 1 \n'
             '(Port 1)TX[10]: 05 64 05 C0 01 00 0A 00 (E0 8C)-CRC\n')
-        input_data = input_data.replace(' ', '')
-        output_data = [
+        output_data = input_data.replace(' ', '')
+        expected_output = [
             (
                 '056405C001000A00',
                 'E08C',
@@ -121,21 +121,21 @@ class TestParseInput(unittest.TestCase):
             )
         ]
         exit_value = parseInput.parseData('', input_data)
-        self.assertEqual(exit_value, output_data)
+        self.assertEqual(exit_value, expected_output)
 
 
     def test_pasted_rx_message_with_message_bytes_and_parens_around_crc_and_leading_tx(self):
         'Verifies the result for a pasted message with leading TX[xx]: and parenthesis around the crc bytes'
         input_data = ('RX[17]: 05 64 0A 44 83 00 BF 00 (B1 4A)-CRC\n'
                       'E9 C1 81 00 00 (66 44)\n')
-        input_data = input_data.replace(' ', '')
-        open_paren_location = input_data.find('(')
-        close_paren_location = input_data.find(')')
-        colon_location = input_data.find(':')
-        output_data = [
+        output_data = input_data.replace(' ', '')
+        open_paren_location = output_data.find('(')
+        close_paren_location = output_data.find(')')
+        colon_location = output_data.find(':')
+        expected_output = [
             (
-                input_data[colon_location + 1:open_paren_location],
-                input_data[open_paren_location + 1:close_paren_location],
+                output_data[colon_location + 1:open_paren_location],
+                output_data[open_paren_location + 1:close_paren_location],
                 "False"
             ),
             (
@@ -145,7 +145,7 @@ class TestParseInput(unittest.TestCase):
             )
         ]
         exit_value = parseInput.parseData(input_data, '')
-        self.assertEqual(exit_value, output_data)
+        self.assertEqual(exit_value, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
